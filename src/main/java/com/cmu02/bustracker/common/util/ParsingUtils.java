17526d934315707e97f207500c47835978212a85
HyeonJun <hyeonjun1968@naver.com>
2026-05-10 23:30:12 +0900

    refactor: 생성자 Lombok으로 수정

--- src/main/java/com/cmu02/bustracker/common/util/ParsingUtils.java
+++ src/main/java/com/cmu02/bustracker/common/util/ParsingUtils.java
@@ -1,15 +1,15 @@
 package com.cmu02.bustracker.common.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * 문자열 파싱 공통 유틸리티.
  * <p>Seoul API 응답값은 숫자임에도 null 또는 빈 문자열로 오는 경우가 있어
  * 안전한 변환 메서드를 한 곳에서 관리한다.</p>
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ParsingUtils {
-
-    private ParsingUtils() {
-    }
-
     /**
      * 문자열을 int로 변환한다. null, 빈 문자열, 변환 실패 시 0을 반환한다.
      */
